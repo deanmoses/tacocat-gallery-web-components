e40d36cecf6e0c1f8b1e7c993b4ade0a2e76f57b
Dean Moses <moses@tacocat.com>
2021-12-26 16:59:18 -0800

    Header doesn't require adding the hidden-sm class
    Figured out how to add stuff to the shadow DOM host element

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,20 @@
 const template = document.createElement('template');
 template.innerHTML = `
-<aside><slot></slot></aside>
+<style>
+    h2 {
+        display: none;
+    }
+</style>
+<h2 role="heading" aria-level="2"><slot name="title"></slot></h2>
+<slot name="content"></slot>
 `
 
 customElements.define('tacocat-sidebar', class extends HTMLElement {
     connectedCallback() {
         let shadowRoot = this.attachShadow({ mode: 'open' });
         shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.shadowRoot.host.classList.add("hidden-sm");
+        this.shadowRoot.host.setAttribute("role", "section");
     }
 });
