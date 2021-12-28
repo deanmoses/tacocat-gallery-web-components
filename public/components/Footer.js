ba46704bea31d795ae9c0aad552aa3a0b92538c6
Dean Moses <moses@tacocat.com>
2021-12-28 13:47:44 -0800

    Tacocat logo in footer is now a webp

--- public/components/Footer.js
+++ public/components/Footer.js
@@ -6,7 +6,16 @@ template.innerHTML = `
         padding-top: var(--default-padding);
     }
 </style>
-<img src="images/tacocat-logo.png" width="102px" height="19px" />
+<picture>
+    <source srcset="images/tacocat-logo.webp" />
+    <source srcset="images/tacocat-logo.png" />
+    <img
+        src="images/tacocat-logo.png"
+        width="102px"
+        height="19px"
+        alt="Tacocat Logo"
+    />
+</picture>
 `
 customElements.define('tacocat-footer', class extends HTMLElement {
 
